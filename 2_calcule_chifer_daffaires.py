287abba4f0f0432a690c7c3f11444af2a7cc7044
SADEK <mohamed20000327@gmail.com>
2024-04-13 01:35:17 +0100

    updat

---
+++ 2_calcule_chifer_daffaires.py
@@ -0,0 +1,7 @@
+PV = int ( input("Veuillez entre la quntite de produit vendus : "))
+
+Prix = float ( input("Veuillez entre le prix de vente : "))
+
+CA = PV * Prix 
+
+print("Le chiffer d'affaires de l'entreprise est : " , CA)
---
+++ 2_sum.py
@@ -0,0 +1,7 @@
+
+A = float ( input( " Veuillez entrer la valeur de A."))
+B = float ( input( " Veuillez entrer la valeur de B."))
+
+S = A + B 
+
+print (" La somme de A et B c'est : " ,S )
