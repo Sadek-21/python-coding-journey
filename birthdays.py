6934cad296bc00f2de1b8b91af920c3570e8cc3b
SADEK <mohamed20000327@gmail.com>
2024-05-16 16:42:18 +0000

    first add

--- birthdays.py
+++ birthdays.py
@@ -11,6 +11,7 @@ while True:
         print(birthdays[name] + 'is the birthday of ' + name)
     else:
                 print('I do not have birthday information for ' + name)
+                print('What is their birthday?')
                 bday = input()
                 birthdays[name] = bday
                 print('Birthday database updated.')
