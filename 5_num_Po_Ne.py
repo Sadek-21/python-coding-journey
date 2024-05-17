124705f0842397da82fded69a21730b493d846c2
SADEK <mohamed20000327@gmail.com>
2024-05-17 12:56:12 +0000

    first add

--- 5_num_Po_Ne.py
+++ 5_num_Po_Ne.py
@@ -1,6 +1,7 @@
 print (" Programme qui determine la nature d'un nomber :")
 
 A = float( input (" Veuillez saisir le premier number :"))
+
 if A > 0 :
     print(" Ce nomber est positif")
 elif A < 0 :
